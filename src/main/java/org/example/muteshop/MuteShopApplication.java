52f59490e38dab93286ea1ece07fa5486e5a9777
LSG2024 <156012256+LSG2024@users.noreply.github.com>
2025-02-20 00:12:30 -0700

    My crud

---
+++ src/main/java/org/example/muteshop/MuteShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.muteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuteShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuteShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/muteshop/MuteShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.muteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuteShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
